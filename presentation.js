0f6777f3fd621ec251e61aef07492c28d7d6547b
remew <foolishoknct@gmail.com>
2015-07-05 13:50:59 +0900

    version1 complete...?

--- presentation.js
+++ presentation.js
@@ -1,43 +1,63 @@
 
 // - global ---------------------------------------
+var go_prev_button;
+var go_next_button;
+
 var go_prev;
 var go_next;
+
+var slide;
 
 var now_page = 0;
 var page_num = 0;
 
-var sheets_object;
+var duration = 500;
+
+//var debug = true;
+var debug = false;
+
+var default_front_cover = '<div class="default_front_cover"><h1>Hello, <span>Browsentation!!</span></h1><h2><span>Browsentation</span>\'s Default Title</h2></div>';
+var default_back_cover = '<div class="default_back_cover"><h1>Thank you for watching</h1></div>';
 
 window.onload = function() {
-	sheets = $(".sheet");
-	page_num = sheets.length;
-	sheets.eq(0).addClass("current");
-	sheets.eq(1).addClass("next");
-	var go_prev = function() {
+	if( document.getElementById("front_cover") === null ) {
+		$("#sheets").prepend('<div id="front_cover" class="sheet default_front_cover_wrapper">' + default_front_cover + '</div>');
+	}
+	if( document.getElementById("back_cover") === null ) {
+		$("#sheets").append('<div id="back_cover" class="sheet default_back_cover_wrapper">' + default_back_cover + '</div>');
+	}
+	page_num = $(".sheet").length;
+	$("#sheets").css("width", 100 * page_num + "%");
+	$(".sheet").each(function() {
+		$(this).css("width", 100 / page_num + "%");
+	});
+	go_prev = function() {
 		if( now_page != 0 ) {
-			$(".current").addClass("slide_out_rev").removeClass("current").removeClass("slide_in");
-			sheets.eq(now_page - 1).addClass("current").addClass("slide_in_rev");
 			now_page--;
+			slide(now_page);
 		}
 	};
 
-	var go_next = function() {
-		if( now_page != page_num ) {
-			$(".current").addClass("slide_out").removeClass("current").removeClass("slide_in");
-			//$(".next").addClass("current").addClass("slide_in").removeClass("next");
-			sheets.eq(now_page + 1).addClass("current").addClass("slide_in");
+	go_next = function() {
+		if( now_page != page_num - 1 ) {
 			now_page++;
-		} else {
-			alert("www");
+			slide(now_page);
 		}
 	};
+
+	slide = function(index) {
+		$("#sheets").animate({left:-100 * index + "%"}, {duration:duration, easing:"linear"});
+	};
 	go_prev_button = document.getElementById("go_prev");
+	go_prev_button.addEventListener("click", go_prev);
+
 	go_next_button = document.getElementById("go_next");
+	go_next_button.addEventListener("click", go_next);
 
 	now_page = 0;
 
-	document.addEventListener("contextmenu", function(event) {
-		event.preventDefault();
+	document.addEventListener("contextmenu", function(e) {
+		e.preventDefault();
 	}, true);
 	document.getElementById("sheets").addEventListener("mousedown", function(e) {
 		var left_click = 0;
@@ -52,5 +72,20 @@ window.onload = function() {
 				break;
 		}
 	}, true);
+	window.addEventListener("keydown", function(e) {
+		var keyLeft = 37;
+		var keyRight = 39;
+		var keyUp = 38;
+		var keyDown = 40;
+		var keyEnter = 13;
+		
+		if( e.keyCode == keyLeft )	go_prev();
+		if( e.keyCode == keyUp )	go_prev();
+		if( e.keyCode == keyRight )	go_next();
+		if( e.keyCode == keyDown )	go_next();
+		if( e.keyCode == keyEnter )	go_next();
+
+		if(debug) alert(e.keyCode);
+	});
 };
 
